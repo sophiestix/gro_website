7f1f951439f2a93349504306855cd8d9fb0c4712
sophiestix <zsofia.helmeczi@gmail.com>
2017-12-25 22:05:20 +0100

    new init commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 ## Gro's Website for music
+
+### Run development
+run `gulp` and BrowserSync will open the server in the default browser 
---
+++ src/app/app.js
@@ -0,0 +1,24 @@
+require('angular');
+require('angular-ui-router');
+require('angular-aria');
+require('angular-animate');
+require('angular-material');
+require('./components/home/home.js');
+
+var app = angular.module('myApp', ['ui.router', 'ngMaterial', 'myApp.home']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+    .state('home', {
+        url: "/",
+        views: {
+            templateUrl: "app/components/home/home.html"
+        },
+        // "header@home": {
+        //     templateUrl: "app/shared/header/header.html"
+        // }
+    });
+});
